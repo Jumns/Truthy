1173220bf754c323dd5cdecbab1846d73cf9dff2
Nate <heisnathan@outlook.com>
2022-02-13 20:57:45 +0100

    added val graphics

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ AOS.init();
 AOS.init({
   // Global settings:
   startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
-  offset: 100, // offset (in px) from the original trigger point
+  offset: 80, // offset (in px) from the original trigger point
   delay: 500, // values from 0 to 3000, with step 50ms
   duration: 900, // values from 0 to 3000, with step 50ms
   easing: "ease-in-out", // default easing for AOS animations
